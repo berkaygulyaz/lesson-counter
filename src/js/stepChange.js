2f9f684f625bdbeb746bf9630c84fbbf02fd774e
Berkay Gülyaz <berkaygulyaz@gmail.com>
2020-06-24 23:22:57 +0300

    Step transitions

--- src/js/stepChange.js
+++ src/js/stepChange.js
@@ -1,5 +1,24 @@
 class stepChange {
     constructor() {
+        const nextStepBtn = document.querySelector('.next-step-btn');
+        const step = document.querySelectorAll('section')
 
+        nextStepBtn.addEventListener('click', () => {
+            step.forEach(item => {
+                const stepId = item.getAttribute('data-step-id')
+                const selectedId = item.getAttribute('data-selected-id')
+
+                if(stepId === selectedId) {
+                    item.classList.remove('current-step')
+                    item.classList.add('prev-step')
+                } else if (stepId < selectedId) {
+                    item.classList.remove('prev-step')
+                    item.classList.add('prev-step')
+                } else if (stepId > selectedId) {
+                    item.classList.remove('next-step')
+                    item.classList.add('next-step')
+                }
+            })
+        })
     }
 }
