5f6975cfa05ae73298d57cb06ef91a2f73c2e04e
Berkay Gülyaz <berkaygulyaz@gmail.com>
2020-07-15 20:58:49 +0300

    Version 1 completed

--- src/js/stopwatch.js
+++ src/js/stopwatch.js
@@ -7,15 +7,43 @@ class Stopwatch {
     const startBtn = document.querySelector('.start-btn');
     const stopBtn = document.querySelector('.stop-btn');
     const body = document.querySelector('body');
+    const audio = document.querySelector('.audio')
+
+    const workTime = 1500 ;
+    const breakTime = 300;
+    let timer = 0;
+    
+
 
     startBtn.addEventListener('click', () => {
       this.timerInterval();
       body.classList.add('start-timer');
+
+      setInterval(() => {
+        timer++
+
+        if(timer == workTime) {
+          audio.play();
+          body.classList.remove('start-timer');
+          body.classList.add('break-timer');
+          body.classList.add('break-alert');
+          timer = 0;
+        } else if (timer == breakTime && body.classList.contains('break-alert')) {
+          audio.play();
+          body.classList.add('start-timer');
+          body.classList.remove('break-timer');
+          body.classList.remove('break-alert');
+          timer = 0
+        }
+      }, 1000);
+
+
     });
 
     stopBtn.addEventListener('click', () => {
-      this.timerClear();
-      body.classList.remove('start-timer')
+      this.timerClear(this.interval);
+      body.classList.remove('start-timer', 'break-timer')
+      audio.pause();
     });
   }
 
